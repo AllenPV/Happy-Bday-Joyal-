f4a05fb48313ca9c9b20ef0ca9fa9970dbf0136d
AllenPV <73430174+AllenPV@users.noreply.github.com>
2021-07-05 12:58:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var sound = document.getElementById("Happy-Birthday-To-You-Happy-Birthday-Songs-2021-WishesPlus.com_.mp3"); 
+function click(){
+    sound.play();
+    
+}
+function stop(){
+    sound.stop();
+}
